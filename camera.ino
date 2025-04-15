a99880ef9b7c582d90b3195156af960470a9d3af
NicholaChen <150370986+NicholaChen@users.noreply.github.com>
2025-04-14 20:28:00 -0500

    Update camera.ino

--- camera.ino
+++ camera.ino
@@ -6,6 +6,8 @@
 #include "soc/rtc_cntl_reg.h"  // Disable brownour problems
 #include "driver/rtc_io.h"
 
+
+#include "RTClib.h"
 // define the number of bytes you want to access
 
 // Pin definition for CAMERA_MODEL_AI_THINKER
@@ -29,10 +31,16 @@
 
 int pictureNumber = 0;
 
+
+const byte numChars = 128;
+char receivedChars[numChars];
+
+int newData = 0;
+
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
  
-  Serial.begin(115200);
+  Serial.begin(19200);
   //Serial.setDebugOutput(true);
   //Serial.println();
   
@@ -71,30 +79,95 @@ void setup() {
   // Init Camera
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
+    //Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
   
   //Serial.println("Starting SD Card");
-  if(!SD_MMC.begin()){
-    Serial.println("SD Card Mount Failed");
+  if(!SD_MMC.begin("/sdcard", true)){
+    //Serial.println("SD Card Mount Failed");
     return;
   }
   
   uint8_t cardType = SD_MMC.cardType();
   if(cardType == CARD_NONE){
-    Serial.println("No SD Card attached");
+    //Serial.println("No SD Card attached");
     return;
+  }
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+
+  while (true) {
+    recvWithStartEndMarkers();
+    if (newData > 0) {
+      if (newData == 1 && receivedChars[0]==0) {
+        Serial.write((uint8_t) 0);
+        break;
+      }
+      newData = 0;
+    }
   }
 }
 
+static boolean recvInProgress = false;
+
+static uint8_t startNum = 0;
+static uint8_t endNum = 0;
+static byte ndx = 0;
+char startMarker = '<';
+char endMarker = '>';
+uint8_t rc;
+
+void recvWithStartEndMarkers() {
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+    if (recvInProgress == true) {
+        
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+          ndx = numChars - 1;
+      }
+      if (rc == 254) {
+        endNum += 1;
+
+        if (endNum == 4) {
+          endNum = 0;
+          recvInProgress = false;
+          
+          newData = ndx-4;
+          ndx = 0;
+        }
+      } else if (endNum > 0) {
+        endNum = 0;
+      }
+    }
+    else if (rc == 255) {
+      startNum += 1;
+      if (startNum == 4)  {
+        recvInProgress = true;
+        startNum = 0;
+      }
+    } else if (startNum > 0) {
+      startNum = 0;
+    }
+  }
+}
+String path = "/Picture";
+int repeat = 0;
+
+
 void loop() {
+  Serial.write((uint8_t) 0);
+  Serial.setDebugOutput(false);
+
   camera_fb_t * fb = NULL;
   
   // Take Picture with Camera
   fb = esp_camera_fb_get();  
   if(!fb) {
-    Serial.println("Camera capture failed");
+    //Serial.println("Camera capture failed");
     return;
   }
 
@@ -103,28 +176,63 @@ void loop() {
   bool converted = frame2bmp(fb, &buf, &buf_len);
   esp_camera_fb_return(fb);
   if(!converted){
-      Serial.println("BMP conversion failed");
-      return;
+    //Serial.println("BMP conversion failed");
+    return;
   }
 
-  // Path where new picture will be saved in SD Card
-  String path = "/picture" + String(pictureNumber) +".bmp";
+  String new_path = "";
+  while (true) {
+    if (!Serial.available()) {
+      break;
+    }
+    recvWithStartEndMarkers();
+    if (newData > 0) {
+      if (newData == 1 && receivedChars[0]==0) {
+        Serial.write((uint8_t) 0);
+      } else if (newData == sizeof(uint32_t)) {
+
+        uint32_t t;
+        memcpy(&t, receivedChars, sizeof(uint32_t));
+
+        DateTime dt  = DateTime(t);
+
+        char format[] = "YYMM_DD_hh_mm_ss";
+
+        new_path = "/"+String(dt.toString(format));
+      }
+      newData = 0;
+    }
+  }
+  
+  if (new_path == "" || new_path == path) {
+    new_path = path + "_" + String(repeat);
+
+    repeat += 1;
+  } else {
+    path = new_path;
+
+    repeat = 0;
+  }
+  new_path = new_path + ".bmp";
+  newData = 0;
+  recvInProgress = false;
+
+  startNum = 0;
+  endNum = 0;
 
   fs::FS &fs = SD_MMC; 
-  Serial.printf("Picture file name: %s\n", path.c_str());
-  
-  File file = fs.open(path.c_str(), FILE_WRITE);
+  //Serial.printf("Picture file name: %s\n", path.c_str());
+
+  File file = fs.open(new_path.c_str(), FILE_WRITE);
   if(!file){
-    Serial.println("Failed to open file in writing mode");
+    //Serial.println("Failed to open file in writing mode");
   } 
   else {
     file.write(buf, buf_len); // payload (image), payload length
-    Serial.printf("Saved file to path: %s\n", path.c_str());
+    //Serial.printf("Saved file to path: %s\n", path.c_str());
   }
   file.close();
   esp_camera_fb_return(fb); 
   free(buf);
   pictureNumber += 1;
-
-  delay(1000);
 }
